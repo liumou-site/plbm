498709ae2d3a0814c600a506876ab8bddb69698e
MagicBook <MagicBook@qq.com>
2022-08-10 18:50:37 +0800

    直接使用率

--- disk/DiskInformation.py
+++ disk/DiskInformation.py
@@ -35,10 +35,11 @@ class DiskInfo:
 		self.base()
 		for dev in self.dev_list:
 			info = self.Partition_information[dev]
-			print(f"总容量: {info.total / 1000 / 1000} MB")
-			print(f"使用量: {info.used / 1000 / 1000} MB")
-			print(f"剩余量: {info.free / 1000 / 1000} MB")
-			print(f"使用率: {str(info.used / info.total * 100)[0:4]} %")
+			print(info)
+			# print(f"总容量: {info.total / 1000 / 1000} MB")
+			# print(f"使用量: {info.used / 1000 / 1000} MB")
+			# print(f"剩余量: {info.free / 1000 / 1000} MB")
+			# print(f"使用率: {str(info.used / info.total * 100)[0:4]} %")
 	# for i in psutil.disk_partitions(all=False):
 	# 	dev = i.device
 	# 	self.dev_list.append()
