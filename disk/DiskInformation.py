e6c9033c74f92900503e410486c7f18baffc047a
MagicBook <MagicBook@qq.com>
2022-08-10 18:50:05 +0800

    直接使用率

--- disk/DiskInformation.py
+++ disk/DiskInformation.py
@@ -39,13 +39,18 @@ class DiskInfo:
 			print(f"使用量: {info.used / 1000 / 1000} MB")
 			print(f"剩余量: {info.free / 1000 / 1000} MB")
 			print(f"使用率: {str(info.used / info.total * 100)[0:4]} %")
-		# for i in psutil.disk_partitions(all=False):
-		# 	dev = i.device
-		# 	self.dev_list.append()
-		# 	paths = i.mountpoint
-		# 	print(f"设备: {dev}")
-		# 	info = psutil.disk_usage(str(paths))
-		# 	print(f"总容量: {info.total / 1000 / 1000} MB")
-		# 	print(f"使用量: {info.used / 1000 / 1000} MB")
-		# 	print(f"剩余量: {info.free / 1000 / 1000} MB")
-		# 	print(f"使用率: {str(info.used / info.total * 100)[0:4]} %")
+	# for i in psutil.disk_partitions(all=False):
+	# 	dev = i.device
+	# 	self.dev_list.append()
+	# 	paths = i.mountpoint
+	# 	print(f"设备: {dev}")
+	# 	info = psutil.disk_usage(str(paths))
+	# 	print(f"总容量: {info.total / 1000 / 1000} MB")
+	# 	print(f"使用量: {info.used / 1000 / 1000} MB")
+	# 	print(f"剩余量: {info.free / 1000 / 1000} MB")
+	# 	print(f"使用率: {str(info.used / info.total * 100)[0:4]} %")
+
+
+if __name__ == "__main__":
+	up = DiskInfo()
+	up.info()
