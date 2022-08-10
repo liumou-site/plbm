cbb56f89ed96a65cb41b3e479009789863d9864c
MagicBook <MagicBook@qq.com>
2022-08-10 18:29:21 +0800

    磁盘

--- disk/DiskInformation.py
+++ disk/DiskInformation.py
@@ -12,9 +12,9 @@
 import psutil
 
 for i in psutil.disk_partitions(all=False):
-	dev = f"/dev/{i.device}"
+	dev = i.device
 	print(f"设备: {dev}")
-	info = psutil.disk_usage(dev)
+	info = psutil.disk_usage(str(dev))
 	print(f"总容量: {info.total / 1024 /1024} MB")
 	print(f"使用量: {info.used / 1024 /1024} MB")
 	print(f"剩余量: {info.free / 1024 /1024} MB")
