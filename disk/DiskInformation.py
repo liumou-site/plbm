0aa447412f0f4a32ad4ccfb6b03468ee520a8a4b
MagicBook <MagicBook@qq.com>
2022-08-10 18:33:22 +0800

    磁盘

--- disk/DiskInformation.py
+++ disk/DiskInformation.py
@@ -12,7 +12,7 @@
 import psutil
 
 for i in psutil.disk_partitions(all=False):
-	dev = i.device
+	dev = i.maxpath
 	print(f"设备: {dev}")
 	info = psutil.disk_usage(str(dev))
 	print(f"总容量: {info.total / 1000 /1000} MB")
@@ -26,8 +26,8 @@ print(f"总容量: {info.total / 1000 / 1000} MB")
 print(f"使用量: {info.used / 1000 / 1000} MB")
 print(f"剩余量: {info.free / 1000 / 1000} MB")
 
-print(f"/dev/nvme0n1p5")
-info = psutil.disk_usage(str('/dev/nvme0n1p5'))
+print(f"/media/liumou/BA18865F18861B11")
+info = psutil.disk_usage(str('/media/liumou/BA18865F18861B11'))
 print(f"总容量: {info.total / 1000 / 1000} MB")
 print(f"使用量: {info.used / 1000 / 1000} MB")
 print(f"剩余量: {info.free / 1000 / 1000} MB")
