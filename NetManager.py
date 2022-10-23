72899117dc58ccb77466e1cea1d354484abac070
meng <liumou.site@qq.com>
2022-10-23 23:00:09 +0800

    增加当前环境检测

--- NetManager.py
+++ NetManager.py
@@ -14,48 +14,46 @@ from cmd import ComMand
 
 
 class NetManager(object):
-	def __init__(self, password=None, ipv4=None, gateway=None,  netmask=24, dns1=None, dns2=None, subnet=None, device=None, log=True):
+	def __init__(self, password=None, ip=None, gw=None, mask=24, dns1=None, dns2=None, net=None, dev=None, log=True):
 		"""
 		网络管理模块,参数均为可选传参，请根据实际需求传入
 		:param password: (str, optional): 设置主机密码. Defaults to None.
-		:param ipv4: (str, optional): 设置IP地址. Defaults to "192.168.1.138".
-		:param gateway: (str, optional): 设置网关. Defaults to "192.168.1.1".
-		:param netmask: (int, optional): 设置子网掩码. Defaults to 24.
+		:param ip: (str, optional): 设置IP地址. Defaults to "192.168.1.138".
+		:param gw: (str, optional): 设置网关. Defaults to "192.168.1.1".
+		:param mask: (int, optional): 设置子网掩码. Defaults to 24.
 		:param dns1: (str, optional): 设置DNS1. Defaults to "114.114.114.114".
 		:param dns2: (str, optional): 设置DNS2. Defaults to "119.29.29.29".
-		:param subnet: (str, optional): 设置网段,一般是自动尝试配置IP需要. Defaults to '192.168.1.'.
-		:param device: (str, optional): 设置网卡名称. Defaults to 'ens33'.
+		:param net: (str, optional): 设置网段,一般是自动尝试配置IP需要. Defaults to '192.168.1.'.
+		:param dev: (str, optional): 设置网卡名称. Defaults to 'ens33'.
 		:param log: (bool, optional): 是否启用日志功能
 		"""
-		if ipv4 is None:
-			ipv4 = "192.168.1.138"
-		self.ipv4 = ipv4
-		#
-		if gateway is None:
-			gateway = "192.168.1.1"
+		if ip is None:
+			ip = "192.168.1.138"
+		if gw is None:
+			gw = "192.168.1.1"
 		if dns1 is None:
 			dns1 = "114.114.114.114"
 		if dns2 is None:
 			dns2 = "119.29.29.29"
-		if subnet is None:
-			subnet = "192.168.1."
-		if device is None:
-			device = 'ens33'
+		if net is None:
+			net = "192.168.1."
+		if dev is None:
+			dev = 'ens33'
 		self.log = log
 		# 主机密码
 		self.password = password
 		# 网段
-		self.subnet = subnet
+		self.subnet = net
 		# IP地址
-		self.ipv4 = ipv4
+		self.ipv4 = ip
 		# 网关
-		self.gateway = gateway
+		self.gateway = gw
 		# DNS1
 		self.dns1 = dns1
 		# DNS2
 		self.dns2 = dns2
 		# 子网掩码
-		self.netmask = netmask
+		self.netmask = mask
 		# 连接名称
 		self.connect_name = 'Y'
 		# 连接模式
@@ -65,30 +63,24 @@ class NetManager(object):
 		# DNS列表
 		self.dns_list = []
 		# 网卡设备
-		self.device = device
+		self.device = dev
 		self.cmd = ComMand(password=password)
 
 	def connect_create(self, name="Y", mode='auto'):
-		"""_summary_
-        创建连接
-        Args:
-            name (str, optional): 连接名称. Defaults to "Y".
-        """
+		"""
+		创建连接
+		:param name: (str, optional): 连接名称. Defaults to "Y".
+		:param mode: (str, optional): 连接模式. Defaults to "auto".
+		:return:
+		"""
 		pass
 
-	def cmd_status(self, cmd):
-		"""
-        执行
-        """
-
-	pass
-
 	def connect_delete(self, name=None):
-		"""_summary_
-        删除连接
-        Args:
-            name (str, optional): _description_. Defaults to "Y".
-        """
+		"""
+		删除连接
+		:param name: name (str, optional): _description_. Defaults to "Y".
+		:return:
+		"""
 		if name is None:
 			name = self.connect_name
 		if self.get_connect_list() and name in self.connect_list:
@@ -96,15 +88,15 @@ class NetManager(object):
 			print("正在删除链接: ", name)
 
 	def get_device_list(self):
-		"""_summary_
-        获取设备列表
-        """
-		pass
+		"""
+		获取设备列表
+		:return:
+		"""
 
 	def get_connect_list(self):
 		"""
-        获取连接列表
-        """
+		获取连接列表
+		"""
 		c = """nmcli con  | grep -v UUID | awk '{print $1}'"""
 		self.connect_list = getoutput(c).split("\n")
 		if len(self.connect_list) == 0:
@@ -114,12 +106,12 @@ class NetManager(object):
 
 	def get_current(self):
 		"""
-        获取当前配置,包含: IP4、网关、掩码
-        """
+		获取当前配置,包含: IP4、网关、掩码
+		"""
 		pass
 
 	def get_dns(self):
 		"""
-        获取DNS配置列表
-        """
+		获取DNS配置列表
+		"""
 		pass
